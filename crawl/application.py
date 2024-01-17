4abec9bc3a9d9278e88a77b277b9f36b2dde9924
y00njae <rladbswo12@gist.ac.kr>
2024-01-17 15:13:51 +0900

    크롤링 로직 변경

--- crawl/application.py
+++ crawl/application.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 
 from crawl.constants import CHAMPION_LIST
+from crawl.sheet import update_csv
 
 
 def get_video_info(crawler, url):
@@ -16,7 +17,6 @@ def get_video_info(crawler, url):
         )
     )
 
-    result = []
     count = 0
     last_page_height = crawler.get_scroll_height()
     while True:
@@ -33,9 +33,10 @@ def get_video_info(crawler, url):
         videos = crawler.crawl_items(
             By.CSS_SELECTOR, "#contents > ytd-rich-grid-row ytd-rich-item-renderer"
         )
-        appended_videos = videos[count:]
-
-        for video in appended_videos:
+        appended_vidoes = videos[count:]
+        count = len(videos)
+        result = []
+        for video in appended_vidoes:
             title = video.find_element(
                 By.CSS_SELECTOR, "yt-formatted-string#video-title"
             ).text
@@ -44,11 +45,8 @@ def get_video_info(crawler, url):
             href = video.find_element(
                 By.CSS_SELECTOR, "ytd-thumbnail a#thumbnail"
             ).get_attribute("href")
+            
             result.append({"title": title, "champion": champion, "href": href})
+        update_csv(result, "output_data/video_data.csv")
+        print(f"크롤링 완료: {count}개의 영상")
 
-        # if len(videos) >= 100:
-        #     break
-        print('현재까지 크롤링한 영상 개수: ', len(videos))
-        count = len(videos)
-
-    return result
